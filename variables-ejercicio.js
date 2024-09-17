1f7e477ede11a9bca984e43974962f8d8e0b8819
Veranno <edusibileau@gmail.com>
2024-09-17 17:55:17 -0300

    mi segundo archivo

---
+++ variables-ejercicio.js
@@ -0,0 +1,15 @@
+let edad = 25;
+let nombre = "el que escribe";
+let meGustaProgramar = true;
+let colores = ["rojo", "azul", "verde"];
+const persona = {
+    nombre: "Hector",
+    edad: 25,
+    ciudad: "Chipre"
+};
+
+console.log (edad);
+console.log (nombre);
+console.log (meGustaProgramar);
+console.log (colores);
+console.log (persona);
